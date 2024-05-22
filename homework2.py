9dd0c449638fc75a7782cb1501b744aa42c1e9ad
ovdom <ovdom@mail.ru>
2024-05-22 23:51:40 +0600

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+lesson = 'Pathon'
+homework = 12
+number_of_hours = 1.5
+average_execution_time = number_of_hours/homework
+print('Курс: ',lesson,', всего задач:', homework, ', затрачено часов: ', number_of_hours, ', среднее время выполнения:', average_execution_time, 'часа')
